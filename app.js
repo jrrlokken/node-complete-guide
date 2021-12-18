1f1d884d7c1e0a67ea0adfa4fb6676f0c30b3c84
Joshua Lokken <joshualokken@pm.me>
2021-12-18 12:21:07 -0600

    update

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const http = require("http");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In a middleware");
+  res.status(200).send({ message: "Add Product" });
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
   res.status(200).send({ message: "success" });
 });
 
-const server = http.createServer(app);
-
-server.listen(3000, () => {
-  console.log("Server listening on 3000....");
+app.listen(3000, () => {
+  console.log("Server listening on 3000...");
 });
